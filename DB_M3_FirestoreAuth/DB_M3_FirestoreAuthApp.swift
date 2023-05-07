e0488d2d28784a8ae89412060faae6dfb783378d
Tom Montgomery <montgomery3681@gmail.com>
2023-05-06 21:47:44 -0600

    Initial Commit

---
+++ DB_M3_FirestoreAuth/DB_M3_FirestoreAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  DB_M3_FirestoreAuthApp.swift
+//  DB_M3_FirestoreAuth
+//
+//  Created by tom montgomery on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct DB_M3_FirestoreAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
